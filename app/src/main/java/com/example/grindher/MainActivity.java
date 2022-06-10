3b39b0df5dee76328752dddaa7a8477f36a92ea1
FloAlbaux <florence.albaux@gmail.com>
2022-06-10 18:30:54 +0200

    First exo print text from user on click button

--- app/src/main/java/com/example/grindher/MainActivity.java
+++ app/src/main/java/com/example/grindher/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.grindher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void btnRegisterClick (View view) {
+        EditText editTextFirstName = findViewById(R.id.editTextFirstName);
+        EditText editTextLastName = findViewById(R.id.editTextLastName);
+        EditText editTextTextEmail = findViewById(R.id.editTextTextEmail);
+
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText("First name : " + editTextFirstName.getText().toString() + "\n " +
+                "Last name : " + editTextLastName.getText().toString() + "\n " +
+                "email : " + editTextTextEmail.getText().toString() + "\n ");
     }
 }
